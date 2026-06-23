ca63613754ce43d9c890bd2cb726712f01609e6c
Aman Sahu <amansahuxy@gmail.com>
2026-06-23 18:00:29 +0530

    Add theme switching with JavaScript

---
+++ index.js
@@ -0,0 +1,10 @@
+let drkBtn=document.getElementById("dark");
+let brtBtn=document.getElementById("bright");
+
+drkBtn.addEventListener("click",()=>{
+    document.body.style.backgroundColor="black"
+})
+
+brtBtn.addEventListener("click",()=>{
+    document.body.style.backgroundColor="white"
+})
